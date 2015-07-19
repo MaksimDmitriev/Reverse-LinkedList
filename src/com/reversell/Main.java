b916376c9022a0e2a1c4a3e287589daddb3d3469
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2015-07-19 23:45:48 +0300

    Add the tests suggested on CodeReview, testing Igal's solution

--- src/com/reversell/Main.java
+++ src/com/reversell/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         singleElemList.print();
-        singleElemList.reversePairs();
+        singleElemList.reversePairs2();
         singleElemList.print();
         
     }
--- src/com/reversell/SinglyLinkedList.java
+++ src/com/reversell/SinglyLinkedList.java
@@ -9,10 +9,10 @@ public class SinglyLinkedList {
         this.mFirst = first;
         this.mLast = first;
     }
-    
-    public SinglyLinkedList(int []array) {
+
+    public SinglyLinkedList(int[] array) {
         if (array == null || array.length == 0) {
-            throw new IllegalArgumentException();
+            return;
         }
         this.mFirst = new Node(array[0]);
         this.mLast = this.mFirst;
@@ -49,34 +49,49 @@ public class SinglyLinkedList {
     }
 
     public void reversePairs() {
-        Node prev = mFirst;
-        Node curr = prev.getNext();
-        Node old = null;
-        while (curr != null) {
-            Node next = curr.getNext();
-            curr.setNext(prev);
-            prev.setNext(null);
-            if (prev == mFirst) {
-                mFirst = curr;
+        if (mFirst == null || mFirst.getNext() == null) {
+            return;
+        }
+        Node temp = null;
+        Node fop = mFirst;
+        mFirst = mFirst.getNext();
+        while (fop != null && fop.getNext() != null) {
+            if (temp != null) {
+                // Set the next element of temp, where temp is the predecessor of fop.
+                temp.setNext(fop.getNext());
             }
-            if (old != null) {
-                old.setNext(curr);
-            }
-            old = prev;
-            prev = next;
-            if (prev != null) {
-                curr = prev.getNext();
-                if (curr == null) {
-                    old.setNext(prev);
-                    mLast = prev;
-                }
-            } else {
-                mLast = old;
-                break;
-            }
+            temp = fop.getNext();
+            
         }
     }
-
+/*
+    
+            public void SwapPairsEnc()
+        {
+            if (First == null || First.Next == null)
+                return;
+            Node Temp = null;
+            Node FirstOfPair = First;
+            mFirst = First.Next;
+            while (FirstOfPair != null && FirstOfPair.Next != null)
+            {
+                //Previos connect to second of pair. First action (picture)
+                if (Temp != null)
+                    Temp.Next = FirstOfPair.Next;
+                //Save second of pair
+                Temp = FirstOfPair.Next;
+                //Connect the rest of list to the first element
+                FirstOfPair.Next = Temp.Next;
+                //Connect the first element to the second
+                Temp.Next = FirstOfPair;
+                //Step forward
+                FirstOfPair = FirstOfPair.Next;
+                //Save the first of the pair
+                Temp = Temp.Next;
+            }
+        }
+    
+    */
     public void reverse() {
         Node prev = mFirst;
         Node current = mFirst.getNext();
@@ -91,6 +106,40 @@ public class SinglyLinkedList {
                 mFirst = prev;
             }
         }
+    }
+
+    // http://codereview.stackexchange.com/a/96879/23821
+    public void reversePairs2() {
+        if (mFirst == null) {
+            return;
+        }
+        Node sentinel = new Node(0);
+        sentinel.setNext(mFirst);
+
+        Node node = sentinel;
+        while (true) {
+            Node next = node.getNext();
+            if (next == null || next.getNext() == null) {
+                break;
+            }
+            node.setNext(swap(next));
+            node = next;
+            mLast = next;
+        }
+
+        mFirst = sentinel.getNext();
+
+        if (mLast.getNext() != null) {
+            mLast = mLast.getNext();
+        }
+    }
+
+    private Node swap(Node node) {
+        Node next = node.getNext();
+        Node nextnext = next.getNext();
+        node.setNext(nextnext);
+        next.setNext(node);
+        return next;
     }
 
 }
