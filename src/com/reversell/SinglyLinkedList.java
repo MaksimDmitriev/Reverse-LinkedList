6c2463c84435d4430ea9a4293e6247e9e6d7aa16
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2015-07-25 22:05:41 +0300

    Simplify the solution following 200_success' suggestions

--- src/com/reversell/SinglyLinkedList.java
+++ src/com/reversell/SinglyLinkedList.java
@@ -2,135 +2,52 @@ package com.reversell;
 
 public class SinglyLinkedList {
 
-    private Node mFirst;
-    private Node mLast;
-    private int mSize;
+    /** Dummy node */
+    private final Node head = new Node(0);
 
-    public SinglyLinkedList(Node first) {
-        this.mFirst = first;
-        this.mLast = first;
-        this.mSize = 1;
-    }
-
-    public SinglyLinkedList(int[] array) {
-        if (array == null || array.length == 0) {
-            return;
-        }
-        this.mFirst = new Node(array[0]);
-        this.mLast = this.mFirst;
-        mSize = 1;
-        for (int i = 1; i < array.length; i++) {
-            addLast(new Node(array[i]));
+    public SinglyLinkedList(int[] data) {
+        for (int i = data.length - 1; i >= 0; i--) {
+            addFirst(data[i]);
         }
     }
 
-    public int getSize() {
-        return mSize;
-    }
-
-    public void addLast(Node node) {
-        mLast.setNext(node);
-        mLast = node;
-        mSize++;
-    }
-
-    public Node getFirst() {
-        return mFirst;
-    }
-
-    public Node getLast() {
-        return mLast;
-    }
-
-    public void print() {
-        Node current = mFirst;
-        System.out.print("[");
-        while (current != null) {
-            System.out.print(current);
-            current = current.getNext();
-            if (current != null) {
-                System.out.print(", ");
-            }
-        }
-        System.out.print("]");
-        System.out.println();
+    public void addFirst(int datum) {
+        Node n = new Node(datum);
+        n.setNext(head.getNext());
+        head.setNext(n);
     }
 
     public void reversePairs() {
-        // The list is empty or contains one element
-        if (mFirst == null || mFirst.getNext() == null) {
-            return;
-        }
-        Node temp = null;
-        Node fop = mFirst;
-        mFirst = mFirst.getNext();
-        while (fop != null && fop.getNext() != null) {
-            if (temp != null) {
-                // Set the next element of temp, where temp is the predecessor of fop.
-                temp.setNext(fop.getNext());
-            }
-            // 12 -> 34 -> 88
-            temp = fop.getNext(); // temp == 34
-            fop.setNext(temp.getNext()); // 12 -> 88
-            temp.setNext(fop); // 34 -> 12
-            temp = temp.getNext(); // temp == 12
-            fop = temp.getNext(); // fop == 88
-            if (fop == null) {
-                mLast = temp;
-            } else {
-                mLast = fop;
-            }
+        // For each loop iteration, transform
+        //
+        //                    maybe null --+
+        //                                 |
+        //                                 v
+        // Original: (a) ->  b  ->  c  ->  d ...
+        // To:        a  ->  c  -> (b) ->  d ...
+        //                          ^
+        //                          |
+        //                          +-- becomes "a" of the next iteration
+
+        Node a = this.head, b, c;
+        while ((b = a.getNext()) != null && (c = b.getNext()) != null) {
+            Node d = c.getNext();
+            a.setNext(c);
+            c.setNext(b);
+            b.setNext(d);
+            a = b;
         }
     }
 
-    public void reverse() {
-        Node prev = mFirst;
-        Node current = mFirst.getNext();
-        prev.setNext(null);
-        while (current != null) {
-            Node next = current.getNext();
-            current.setNext(prev);
-            prev = current;
-            current = next;
-            if (current == null) {
-                mLast = mFirst;
-                mFirst = prev;
-            }
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder("[");
+        for (Node n = this.head.getNext(); n != null; n = n.getNext()) {
+            s.append(n);
+            s.append(" -> ");
         }
-    }
-
-    // http://codereview.stackexchange.com/a/96879/23821
-    public void reversePairs2() {
-        if (mFirst == null) {
-            return;
-        }
-        Node sentinel = new Node(0);
-        sentinel.setNext(mFirst);
-
-        Node node = sentinel;
-        while (true) {
-            Node next = node.getNext();
-            if (next == null || next.getNext() == null) {
-                break;
-            }
-            node.setNext(swap(next));
-            node = next;
-            mLast = next;
-        }
-
-        mFirst = sentinel.getNext();
-
-        if (mLast.getNext() != null) {
-            mLast = mLast.getNext();
-        }
-    }
-
-    private Node swap(Node node) {
-        Node next = node.getNext();
-        Node nextnext = next.getNext();
-        node.setNext(nextnext);
-        next.setNext(node);
-        return next;
+        if (s.length() >= 4) s.setLength(s.length() - 4); // Remove final " -> "
+        return s.append(']').toString();
     }
 
 }
